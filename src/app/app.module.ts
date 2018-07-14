2b0b4074fa1e1654847b196f6a73818339b6533f
bayat <turtbayev@gmail.com>
2018-07-15 00:45:03 +0600

    added Cordova and Ionic Native plugins for Storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {ErrorHandler, NgModule} from '@angular/core';
 import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {IonicStorageModule} from '@ionic/storage';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {StatusBar} from '@ionic-native/status-bar';
 
@@ -26,7 +27,8 @@ import {SavePage} from '../pages/save/save';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
